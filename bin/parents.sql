3225f643b96f2adcd5420962285c1b1dc81df57f
aaron-lebo <aaron.m.lebo@gmail.com>
2014-03-14 15:43:09 -0500

    Delete parents.sql

--- bin/parents.sql
+++
@@ -1,5 +0,0 @@
-WITH RECURSIVE parents AS (
-	SELECT *, 0 as level FROM revisions WHERE project_id = ? and parent is null
-   	UNION ALL
-  	SELECT revisions.*, parents.level + 1 as level FROM revisions JOIN parents ON revisions.project_id = parents.project_id and revisions.parent = parents.number)
-SELECT * FROM parents
