556121b6085a7552c4b84114ab15beff93d9d471
Aaron Lebo <aaron.m.lebo@gmail.com>
2014-03-14 15:46:39 -0500

    delete bin

--- bin/public/bootstrap.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var converter = Markdown.getSanitizingConverter();
-	var editor = new Markdown.Editor(converter);
-    editor.run();
-            
-    var converter2 = Markdown.getSanitizingConverter();
-	var editor2 = new Markdown.Editor(converter2, "-2");
-    editor2.run();
-})()
